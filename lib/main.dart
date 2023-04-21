01e16dbf842c5a946d3813d402593e33b742db20
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-04-20 23:56:31 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:organizing_finances/src/app_widget.dart';
 
-void main() {
-  runApp(AppWidget());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const AppWidget());
 }
