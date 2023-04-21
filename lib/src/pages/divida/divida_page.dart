5f27a2b0dedb39116d752eb0bbd7cf50a34a19b5
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-04-21 04:01:59 -0300

    Update Divida Page

--- lib/src/pages/divida/divida_page.dart
+++ lib/src/pages/divida/divida_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:organizing_finances/src/pages/divida/controllers/divida_controller_impl.dart';
 
 class DividaPage extends StatefulWidget {
   const DividaPage({super.key});
@@ -8,6 +9,8 @@ class DividaPage extends StatefulWidget {
 }
 
 class _DividaPageState extends State<DividaPage> {
+  final controller = DividaControllerImpl();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +27,44 @@ class _DividaPageState extends State<DividaPage> {
         ),
         backgroundColor: Colors.black,
         foregroundColor: const Color.fromRGBO(231, 101, 8, 10),
+      ),
+      body: FutureBuilder<List<Map>>(
+        future: controller.getDividas(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Erro ao Carregar as Dívidas acumuladas');
+          } else if (snapshot.hasData) {
+            final list = snapshot.data;
+            return ListView.builder(
+              itemCount: list?.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(list?[index]['TítuloDívida']),
+                subtitle: Text('R\$ ${(list?[index]['Valor']).toString()}'),
+                trailing: SizedBox(
+                  width: 100,
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {},
+                        color: const Color.fromRGBO(231, 101, 8, 10),
+                        icon: const Icon(Icons.edit),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        color: const Color.fromRGBO(231, 101, 8, 10),
+                        icon: const Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
